e0b01d4e8ee1c53b882112e7439841a9de0da469
钱程 <qiancheng@moseeker.com>
2021-01-14 18:29:53 +0800

    all

--- ws-spring/demo-spring-cloud/netflix-eureka-client/src/main/java/jd/demo/spring/cloud/netflix/eureka/client/NetflixEurekaClientApplication.java
+++ ws-spring/demo-spring-cloud/netflix-eureka-client/src/main/java/jd/demo/spring/cloud/netflix/eureka/client/NetflixEurekaClientApplication.java
@@ -1,13 +1,16 @@
 package jd.demo.spring.cloud.netflix.eureka.client;
 
+import com.netflix.discovery.shared.transport.decorator.EurekaHttpClientDecorator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class NetflixEurekaClientApplication {
-
     public static void main(String[] args) {
+        com.netflix.discovery.DiscoveryClient l ;
+
+        EurekaHttpClientDecorator eurekaHttpClientDecorator;
         SpringApplication.run(NetflixEurekaClientApplication.class, args);
     }
 
