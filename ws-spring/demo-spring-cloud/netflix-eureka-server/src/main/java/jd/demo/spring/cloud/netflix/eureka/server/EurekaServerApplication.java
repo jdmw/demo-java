42915596c0c4df04e7a5c937e2bc8421a51e763b
钱程 <qiancheng@moseeker.com>
2021-01-29 18:23:05 +0800

    all

--- ws-spring/demo-spring-cloud/netflix-eureka-server/src/main/java/jd/demo/spring/cloud/netflix/eureka/server/EurekaServerApplication.java
+++ ws-spring/demo-spring-cloud/netflix-eureka-server/src/main/java/jd/demo/spring/cloud/netflix/eureka/server/EurekaServerApplication.java
@@ -1,13 +1,16 @@
 package jd.demo.spring.cloud.netflix.eureka.server;
 
 import com.netflix.eureka.registry.AbstractInstanceRegistry;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+
+    // change port,run: EurekaServerApplication -Dserver.port=8762 -Deureka.client.registerWithEureka=true -Deureka.client.serviceUrl.defaultZone=http://localhost:8761/eureka,http://localhost:8762/eureka
 
     public static void main(String[] args) {
         // service registry
@@ -15,13 +18,11 @@ public class EurekaServerApplication {
         // renew
         //  step 1 get instance of by serviceId, renewLeaseInfo
         //  step 2 replicateToPeers : com.netflix.eureka.registry.PeerAwareInstanceRegistryImpl.replicateToPeers()
-        SpringApplication.run(EurekaServerApplication.class, args);
-
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(WebApplicationType.SERVLET).run(args);
 
         // get Instances registered with Eureka : http://localhost:8761/
         // org.springframework.cloud.netflix.eureka.server.EurekaController.populateApps
         //  -> invoke PeerAwareInstanceRegistryImpl.getSortedApplications()
-
 
         //com.netflix.eureka.registry.PeerAwareInstanceRegistryImpl.replicateToPeers()
     }
